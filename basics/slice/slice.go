987dbf3c884c0a9f7dc27763b088d4da176d1613
xr <192319969@qq.com>
2025-10-11 18:58:50 +0800

    slice example

---
+++ basics/slice/slice.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(len(s))
+
+	func(s *[]int) {
+		*s = append(*s, 6)
+		fmt.Println(len(*s))
+	}(&s)
+
+	fmt.Println(len(s))
+}
